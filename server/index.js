17ed52094b63556b8845ba47b46aac3fabe6e9a4
Prajwal-B05 <prajwal.is20@bmsce.ac.in>
2023-04-08 14:10:41 +0530

    Updated merkle tree and coverted root o Hexadecimal

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
-
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList.json');
+const { toHex } = require('ethereum-cryptography/utils');
 const port = 1225;
 
 const app = express();
@@ -8,19 +10,38 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+
+const merkleTree = new MerkleTree(niceList);
+const root = merkleTree.getRoot();
+const rt = toHex(root);
+const rot = rt.slice(0,2);
+const isInTheList = true;
+
+const MERKLE_ROOT = rot ;
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
+  const name = req.body.name;
+  const prof = req.body.proof;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  if(prof === proof)
+  {console.log( verifyProof(proof, name, root) );}
+
+  if(verifyProof === true)
+  { isInTheList = true;}
+  else
+  { isInTheList = false;}
+
+  if(isInTheList===true) {
     res.send("You got a toy robot!");
   }
   else {
-    res.send("You are not on the list :(");
+    res.send("You are not on the list ");
   }
 });
 
